baaf6edabd41396d5d8541cf7dbc58d9149b3626
lims-rise <lims.rise@gmail.com>
2024-09-04 11:36:14 +0800

    enterolert water module

---
+++ application/models/Enterolert_idexx_water_model.php
@@ -0,0 +1,294 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Enterolert_idexx_water_model extends CI_Model
+{
+
+    public $table = 'enterolert_water_in';
+    public $tableOut = 'enterolert_water_out';
+    public $id = 'id_enterolert_in';
+    public $idOut = 'id_enterolert_out';
+    public $order = 'DESC';
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    // datatables
+    function json() {
+        $this->datatables->select('enterolert_water_in.id_enterolert_in, enterolert_water_in.id_one_water_sample, enterolert_water_in.id_person, ref_person.initial,
+        enterolert_water_in.id_sampletype, ref_sampletype.sampletype, enterolert_water_in.enterolert_barcode, enterolert_water_in.date_sample, enterolert_water_in.time_sample,
+        enterolert_water_in.volume_bottle, enterolert_water_in.dilution');
+        $this->datatables->from($this->table);
+        $this->datatables->join('ref_person', 'enterolert_water_in.id_person = ref_person.id_person', 'left');
+        $this->datatables->join('ref_sampletype', 'enterolert_water_in.id_sampletype = ref_sampletype.id_sampletype', 'left');
+        $this->datatables->where('lab', $this->session->userdata('lab'));
+        $this->datatables->where('enterolert_water_in.flag', '0');
+        $lvl = $this->session->userdata('id_user_level');
+        if ($lvl == 7){
+            $this->datatables->add_column('action', anchor(site_url('Enterolert_idexx_water/read/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-info btn-sm')), 'id_enterolert_in');
+        }
+        else if (($lvl == 2) | ($lvl == 3)){
+            $this->datatables->add_column('action', anchor(site_url('Enterolert_idexx_water/read/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-info btn-sm')) ."
+                ".'<button type="button" class="btn_edit btn btn-info btn-sm" aria-hidden="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>', 'id_enterolert_in');
+        }
+        else {
+            $this->datatables->add_column('action', anchor(site_url('Enterolert_idexx_water/read/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-warning btn-sm')) ."
+            ".'<button type="button" class="btn_edit btn btn-info btn-sm" aria-hidden="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>'." 
+            ".'<button type="button" class="btn_delete btn btn-danger btn-sm" data-id="$1" aria-hidden="true"><i class="fa fa-trash-o" aria-hidden="true"></i></button>', 'id_enterolert_in');
+        }
+        return $this->datatables->generate();
+    }
+
+    function subjson($id) {
+        $this->datatables->select('ewo.id_enterolert_out, ewo.enterolert_barcode, ewo.date_sample, ewo.time_sample, ewo.enterococcus_largewells,  ewo.enterococcus_smallwells, ewo.enterococcus, ewo.remarks, ewo.flag');
+        $this->datatables->from('enterolert_water_out AS ewo');
+        // $this->datatables->join('ref_testing b', 'FIND_IN_SET(b.id_testing_type, a.id_testing_type)', 'left');
+        // $this->datatables->join('ref_barcode c', 'a.sample_id = c.testing_type_id', 'left');
+        $this->datatables->where('ewo.flag', '0');
+        $this->datatables->where('ewo.id_enterolert_in', $id);
+        $this->datatables->group_by('ewo.id_enterolert_out');
+        $lvl = $this->session->userdata('id_user_level');
+        if ($lvl == 7){
+            $this->datatables->add_column('action', anchor(site_url('Enterolert_idexx_water/read2/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-info btn-sm')), 'id_enterolert_out');
+        }
+        else if (($lvl == 2) | ($lvl == 3)){
+            $this->datatables->add_column('action', anchor(site_url('Enterolert_idexx_water/read2/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-info btn-sm')) ."
+                ".'<button type="button" class="btn_edit_det btn btn-info btn-sm" aria-hidden="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>', 'id_enterolert_out');
+        }
+        else {
+            $this->datatables->add_column('action', '<button type="button" class="btn_edit_det btn btn-info btn-sm" aria-hidden="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>'." 
+                ".'<button type="button" class="btn_delete btn btn-danger btn-sm" data-id="$1" aria-hidden="true"><i class="fa fa-trash-o" aria-hidden="true"></i></button>', 'id_enterolert_out');
+        }
+        return $this->datatables->generate();
+    }
+
+    function subjson72($id) {
+        $this->datatables->select('m72.id_moisture72, m72.id_moisture, m72.date_moisture72, m72.time_moisture72, m72.barcode_tray, m72.dry_weight72, m72.dry_weight_persen, m72.comments72, m72.flag');
+        $this->datatables->from('moisture72 AS m72');
+        // $this->datatables->join('ref_testing b', 'FIND_IN_SET(b.id_testing_type, a.id_testing_type)', 'left');
+        // $this->datatables->join('ref_barcode c', 'a.sample_id = c.testing_type_id', 'left');
+        $this->datatables->where('m72.flag', '0');
+        $this->datatables->where('m72.id_moisture', $id);
+        $this->datatables->group_by('m72.id_moisture72');
+        $lvl = $this->session->userdata('id_user_level');
+        if ($lvl == 7){
+            $this->datatables->add_column('action', anchor(site_url('Moisture_content/read2/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-info btn-sm')), 'id_moisture72');
+        }
+        else if (($lvl == 2) | ($lvl == 3)){
+            $this->datatables->add_column('action', anchor(site_url('Moisture_content/read2/$1'),'<i class="fa fa-th-list" aria-hidden="true"></i>', array('class' => 'btn btn-info btn-sm')) ."
+                ".'<button type="button" class="btn_edit_det72 btn btn-info btn-sm" aria-hidden="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>', 'id_moisture72');
+        }
+        else {
+            $this->datatables->add_column('action', '<button type="button" class="btn_edit_det72 btn btn-info btn-sm" aria-hidden="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>'." 
+                     ".'<button type="button" class="btn_delete72 btn btn-danger btn-sm" data-id="$1" aria-hidden="true"><i class="fa fa-trash-o" aria-hidden="true"></i></button>', 'id_moisture72');
+        }
+        return $this->datatables->generate();
+    }
+
+    function get_by_id($id)
+    {
+        $this->db->where($this->id, $id);
+        $this->db->where('flag', '0');
+        // $this->db->where('lab', $this->session->userdata('lab'));
+        return $this->db->get($this->table)->row();
+    }
+
+    function get_by_id_detail($id)
+    {
+        $this->db->where('id_enterolert_out', $id);
+        $this->db->where('flag', '0');
+        // $this->db->where('lab', $this->session->userdata('lab'));
+        return $this->db->get('enterolert_water_out')->row();
+    }
+
+    function get_by_id_detail72($id)
+    {
+        $this->db->where('id_moisture72', $id);
+        $this->db->where('flag', '0');
+        // $this->db->where('lab', $this->session->userdata('lab'));
+        return $this->db->get('moisture72')->row();
+    }
+
+    function get_detail($id)
+    {
+      $response = array();
+      $this->db->select('ewi.id_enterolert_in, ewi.id_one_water_sample, ewi.id_person, rp.initial,
+        ewi.id_sampletype, rs.sampletype, ewi.enterolert_barcode, ewi.date_sample, ewi.time_sample,
+        ewi.volume_bottle, ewi.dilution');
+      $this->db->from('enterolert_water_in AS ewi');
+      $this->db->join('ref_sampletype AS rs', 'ewi.id_sampletype = rs.id_sampletype', 'left');
+      $this->db->join('ref_person AS rp',  'ewi.id_person = rp.id_person', 'left');
+      $this->db->where('ewi.id_enterolert_in', $id);
+      $this->db->where('ewi.flag', '0');
+      $q = $this->db->get();
+
+      if ($q->num_rows() > 0) {
+        $response = $q->row();
+      }
+    
+      return $response;
+    }
+
+    function get_detail2($id)
+    {
+      $response = array();
+      $this->db->select('*');
+      $this->db->where('sample_reception_sample.id_sample', $id);
+      $this->db->where('sample_reception_sample.flag', '0');
+      $q = $this->db->get('sample_reception_sample');
+      $response = $q->row();
+      return $response;
+    }
+
+    // Fuction insert data
+    public function insert($data) {
+        $this->db->insert($this->table,  $data);
+    }
+
+    // Function update data
+    function update($id, $data)
+    {
+        $this->db->where('id_enterolert_in', $id);
+        $this->db->update($this->table, $data);
+    }
+
+    function insert_det($data) {
+        $this->db->insert($this->tableOut, $data);
+        return $this->db->insert_id(); // Return the ID of the newly inserted row
+    }
+
+    function update_det($id, $data) {
+        $this->db->where('id_enterolert_out', $id);
+        $this->db->update('enterolert_water_out', $data);
+    }
+
+    function insert_det72($data) {
+        $this->db->insert($this->table72, $data);
+        return $this->db->insert_id(); // Return the ID of the newly inserted row
+    }
+
+    function update_det72($id, $data) {
+        $this->db->where('id_moisture72', $id);
+        $this->db->update('moisture72', $data);
+    }
+
+    // delete data
+    function delete($id)
+    {
+        $this->db->where($this->id, $id);
+        $this->db->delete($this->table);
+    }
+
+    function delete_detail($id)
+    {
+        $this->db->where('id_enterolert_out', $id);
+        $this->db->delete('enterolert_water_out');
+    }
+    
+
+    function getSampleType(){
+        $response = array();
+        $this->db->select('*');
+        $this->db->where('flag', '0');
+        $this->db->order_by('sampletype', 'DESC');
+        $q = $this->db->get('ref_sampletype');
+        $response = $q->result_array();
+        return $response;
+      }
+
+      function getLabtech() {
+        $response = array();
+        $this->db->select('*');
+        $this->db->where('flag', '0');
+        $this->db->order_by('realname');
+        $labTech = $this->db->get('ref_person');
+        $response = $labTech->result_array();
+        return $response;
+      }
+
+    function getID_one(){
+        $q = $this->db->query('
+        SELECT id_one_water_sample FROM sample_reception
+        WHERE id_one_water_sample NOT IN (SELECT id_one_water_sample FROM enterolert_water_in)
+        AND flag = 0');       
+        $response = $q->result_array();
+        return $response;
+    }
+
+    function getOneWaterSampleById($idOneWaterSample)
+    {
+        $this->db->select('sr.id_sampletype, rs.sampletype');
+        $this->db->from('sample_reception sr');
+        $this->db->join('ref_sampletype rs', 'sr.id_sampletype = rs.id_sampletype', 'left');
+        $this->db->where('sr.id_one_water_sample', $idOneWaterSample);
+        $query = $this->db->get();
+        return $query->row_array();
+    }
+
+    function validate24($id){
+        $q = $this->db->query('
+        SELECT barcode_moisture_content FROM moisture_content
+        WHERE barcode_moisture_content = "'.$id.'"
+        AND barcode_moisture_content NOT IN (SELECT barcode_tray FROM moisture24 WHERE flag IN (0))
+        ');        
+        $response = $q->result_array();
+        return $response;
+    }
+
+    function validate72($id){
+        $q = $this->db->query('
+        SELECT barcode_tray FROM moisture24
+        WHERE barcode_tray = "'.$id.'"
+        AND flag = 0 
+        ');        
+        $response = $q->result_array();
+        return $response;
+    }
+
+    function get_all()
+    {
+        $this->db->select('mc.id_one_water_sample, rp.initial, mc.date_start, rs.sampletype, mc.barcode_moisture_content, mc.tray_weight,
+        mc.traysample_wetweight, mc.time_incubator,mc.comments, m24.date_moisture24, m24.time_moisture24, m24.dry_weight24, m24.comments24,
+        m72.date_moisture72, m72.time_moisture72, m72.dry_weight72, m72.dry_weight_persen, m72.comments72');
+        $this->db->from('moisture_content AS mc');
+        $this->db->join('ref_person AS rp', 'mc.id_person = rp.id_person');
+        $this->db->join('ref_sampletype AS rs', 'mc.id_sampletype = rs.id_sampletype');
+        $this->db->join('moisture24 AS m24', 'mc.id_moisture = m24.id_moisture');
+        $this->db->join('moisture72 AS m72', 'mc.id_moisture = m72.id_moisture');
+        $this->db->where('mc.flag', '0');
+        $this->db->order_by('mc.date_start', 'ASC');
+
+        return $this->db->get()->result();
+    }
+
+    function validateBarcodeMoistureContent($id){
+        $q = $this->db->query('
+        SELECT barcode_moisture_content FROM moisture_content
+        WHERE barcode_moisture_content = "'.$id.'"
+        AND flag = 0 
+        ');        
+        $response = $q->result_array();
+        return $response;
+    }
+
+    function data_chart($valueLargeWells, $valueSmallWells){
+        $q = $this->db->query('
+        select mpn FROM idexxchart WHERE big = "'.$valueLargeWells.'" AND small = "'.$valueSmallWells.'"
+        ');        
+        $response = $q->result_array();
+        return $response;
+      }
+    
+
+      
+}
+
+/* End of file Tbl_delivery_model.php */
+/* Location: ./application/models/Tbl_delivery_model.php */
+/* Please DO NOT modify this information : */
+/* Generated by Harviacode Codeigniter CRUD Generator 2022-12-14 03:38:42 */
+/* http://harviacode.com */
--- application/views/user/tbl_user_list.php
+++ application/views/user/tbl_user_list.php
@@ -10,7 +10,7 @@
         
         <div class="box-body">
         <div style="padding-bottom: 10px;">
-        <?php echo anchor(site_url('user/create'), '<i class="fa fa-wpforms" aria-hidden="true"></i> New User', 'class="btn btn-danger"'); ?>
+        <?php echo anchor(site_url('user/create'), '<i class="fa fa-wpforms" aria-hidden="true"></i> New User', 'class="btn btn-primary"'); ?>
 		<?php //echo anchor(site_url('user/excel'), '<i class="fa fa-file-excel-o" aria-hidden="true"></i> Export Ms Excel', 'class="btn btn-success btn-sm"'); ?>
 		<?php //echo anchor(site_url('user/word'), '<i class="fa fa-file-word-o" aria-hidden="true"></i> Export Ms Word', 'class="btn btn-primary btn-sm"'); ?></div>
         <table class="table table-bordered table-striped" id="mytable">
@@ -30,6 +30,30 @@
                     </div>
             </div>
             </div>
+
+        <!-- MODAL CONFIRMATION DELETE -->
+        <div class="modal fade" id="confirm-modal" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header" style="background-color: #dd4b39; color: white;">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true" style="color: white;">&times;</button>
+                        <h4 class="modal-title"><i class="fa fa-trash"></i> User Setting | Delete <span id="my-another-cool-loader"></span></h4>
+                    </div>
+                    <div class="modal-body">
+                        <div id="confirmation-content">
+                            <div class="modal-body">
+                                <p class="text-center" style="font-size: 15px;">Are you sure you want to delete ID <span id="id" style="font-weight: bold;"></span> ?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer clearfix">
+                        <button type="button" id="confirm-save" class="btn btn-danger"><i class="fa fa-trash"></i> Yes</button>
+                        <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> No</button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
     </section>
 </div>
         <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
@@ -37,6 +61,41 @@
         <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
         <script type="text/javascript">
             $(document).ready(function() {
+
+                function showConfirmation(url) {
+                    deleteUrl = url; // Set the URL to the variable
+                    $('#confirm-modal').modal('show');
+                }
+
+                // Handle the delete button click
+                $(document).on('click', '.btn_delete', function() {
+                    let id = $(this).data('id');
+                    let url = '<?php echo site_url('User/delete'); ?>/' + id;
+                    $('#confirm-modal #id').text(id);
+                    console.log(id);
+                    showConfirmation(url);
+                });
+
+                // When the confirm-save button is clicked
+                $('#confirm-save').click(function() {
+                    $.ajax({
+                        url: deleteUrl,
+                        type: 'POST',
+                        dataType: 'json',
+                        success: function(response) {
+                            if (response.status === 'success') {
+                                alert(response.message);
+                            } else {
+                                alert(response.message);
+                            }
+                        },
+                        complete: function() {
+                            $('#confirm-modal').modal('hide');
+                            location.reload();
+                        }
+                    });
+                });
+
                 $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
                 {
                     return {
