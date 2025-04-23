56887ea153a58347081d83949ad13b1cd8fa17c7
lims-rise <85858263+lims-rise@users.noreply.github.com>
2025-04-23 11:50:15 +0800

    Update index_rep.php
    Fix report

--- application/views/sample_reception/index_rep.php
+++ application/views/sample_reception/index_rep.php
@@ -1,4 +1,66 @@
 <style>
+
+html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+    
+    .content-wrapper {
+        flex: 1;
+        padding-bottom: 20px; /* Space before footer */
+    }
+    
+    footer {
+        width: 100%;
+        margin-top: auto; /* Pushes footer to bottom */
+        padding: 50px 0;
+    }
+
+    .footer-border {
+        width: 100%;
+        border-top: 4px solid #3C8DBC;
+        margin: 0 auto; /* Centers the border */
+    }
+
+    @media print {
+        .noprint {
+            display: none;
+        }
+        @page { 
+            margin: 0; 
+        }
+        body { 
+            margin: 1.6cm; 
+            display: block;
+        }
+        footer {
+            /* width: 100%; */
+            position: fixed;
+            bottom: 0;
+        }
+        .content-wrapper {
+            padding-bottom: 0;
+        }
+    }
+
+  /*   @media print{
+        .noprint{
+            display:none;
+        }
+        @page { margin: 0; }
+        body { margin: 1.6cm; }
+        footer {
+            position: fixed;
+            bottom: 0;
+        }
+    } */
+
 @media print{
     .noprint{
         display:none;
@@ -19,7 +81,8 @@ body { margin: 1.6cm; }
 
 </style>
 
-
+<html>
+<body>
 <div class="content-wrapper">
 
 <section class="content">
@@ -27,46 +90,28 @@ body { margin: 1.6cm; }
     <div class="col-md-12">
         <div class="box box-primary">
             <div class="box-header">
-<!-- <img src="../../../assets/img/header_inv_02.png" width="1025px" class="icon" style="padding: 10px; float: left;"> -->
-<img src="../../../img/onewaterlogo.png" height="40px" class="icon" style="padding: 0px; float: left;">
-<img src="../../../img/monash.png" height="40px" class="icon" style="padding: 0px; float: right;">
-<!-- <h3>LIMS - REPORT</h3> -->
-<div class="noprint">
-    <div class="modal-footer clearfix">
-        <button id='print' class="btn btn-primary no-print" onclick="document.title = '<?php echo 'Print_Spectro_CRM'?>'; window.print();"><i class="fa fa-print"></i> Print</button>
-        <button id='close' class="btn btn-warning" onclick="javascript:history.go(-1);"><i class="fa fa-times"></i> Close</button> 
-    </div>
-</div>
-<!-- // <h4 class=text-right>Invoice : $invoice_number </h4>
-// <h4 class=text-right>Date : $date_invoice </h4> -->
-</div>
-
-<?php
-
-// $q = $this->db->query('SELECT duplication, result, trueness, bias_method FROM obj2b_spectro_crm_det
-// WHERE flag = 0
-// AND id_spec="'.$id_spec.'"
-// ORDER BY duplication');        
-
-// $response = $q->result();
-
-?>
+            <!-- <img src="../../../assets/img/header_inv_02.png" width="1025px" class="icon" style="padding: 10px; float: left;"> -->
+            <img src="../../../img/onewaterlogo.png" height="40px" class="icon" style="padding: 0px; float: left;">
+            <img src="../../../img/monash.png" height="40px" class="icon" style="padding: 0px; float: right;">
+            <!-- <h3>LIMS - REPORT</h3> -->
+            <div class="noprint">
+                <div class="modal-footer clearfix">
+                    <button id='print' class="btn btn-primary no-print" onclick="document.title = '<?php echo 'Print_Spectro_CRM'?>'; window.print();"><i class="fa fa-print"></i> Print</button>
+                    <button id='close' class="btn btn-warning" onclick="javascript:history.go(-1);"><i class="fa fa-times"></i> Close</button> 
+                </div>
+            </div>
+            </div>
 
 
 <div class="box">
-<div style="position: relative; height: 40px; margin-bottom: 10px;">
-  <img src="../../../img/bluebar.png" 
-       style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: 0;" 
-       alt="Background" />
-  <h3 style="position: relative; z-index: 1; margin: 0; line-height: 40px; color: white; text-align: center; font-size: 18px;">
-    CERTIFICATE OF ANALYSIS
-  </h3>
-</div>
-
-<!-- <h3 class="text-center">CERTIFICATE OF ANALYSIS</h3> -->
-<!-- <h3 class="text-center" style="color: white; padding: 10px;">
-  CERTIFICATE OF ANALYSIS
-</h3> -->
+    <div style="position: relative; height: 40px; margin-bottom: 10px;">
+    <img src="../../../img/bluebar.png" 
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: 0;" 
+        alt="Background" />
+    <h3 style="position: relative; z-index: 1; margin: 0; line-height: 40px; color: white; text-align: center; font-size: 18px;">
+        CERTIFICATE OF ANALYSIS
+    </h3>
+    </div>
 
 <input type='hidden' id='id_project' value='<?php echo $id_project; ?>'>
 
@@ -226,23 +271,31 @@ body { margin: 1.6cm; }
     </table>
 </div>
 
-</div>
-</div>
-</div>
-</div>
+</div> <!-- end of div.box -->
+
+</div> <!-- end of box box-primary -->
+</div> <!-- end of col-md-12 -->
+</div> <!-- end of row -->
+<div style="width: 100%; border-top: 5px solid #3C8DBC; margin: 30px 0;"></div>
+
 </section>    
-</div>
+</div> <!-- end of content-wrapper -->
+
+
 <footer>
-<div style="width: 60%; margin: 40px auto 0 auto; border-top: 3px solid black;"></div>
+    <div class="footer-border"></div>
 </footer>
 
+</body>
+</html>
+
 <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
-        <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-        <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
-        <script type="text/javascript">
-            var table
-            $(document).ready(function() {
-                
-               
-            });
-        </script>
+<script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
+<script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
+<script type="text/javascript">
+    var table
+    $(document).ready(function() {
+        
+        
+    });
+</script>
