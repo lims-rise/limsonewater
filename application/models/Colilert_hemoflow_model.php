c8c09cd33235aeabd88aecf92ddb5fcec4d3d6c1
M. Dhiya Ulhaq <dhiyaulhaq@Ms-MacBook-Pro.local>
2026-02-19 00:06:13 +0800

    fix(colilert-hemoflow): add iexx auto calculation

--- application/models/Colilert_hemoflow_model.php
+++ application/models/Colilert_hemoflow_model.php
@@ -228,7 +228,7 @@ class Colilert_hemoflow_model extends CI_Model
 
     function data_chart($valueLargeWells, $valueSmallWells){
         $q = $this->db->query('
-        select mpn FROM idexxchart WHERE big = "'.$valueLargeWells.'" AND small = "'.$valueSmallWells.'"
+        select MPN_mean, MPN_95lo FROM idexx_mpn WHERE count_large = "'.$valueLargeWells.'" AND count_small = "'.$valueSmallWells.'"
         ');        
         $response = $q->result_array();
         return $response;
--- application/views/colilert_hemoflow/index_det.php
+++ application/views/colilert_hemoflow/index_det.php
@@ -286,7 +286,7 @@
                                     <div class="form-group">
                                         <label for="ecoli" class="col-sm-4 control-label">E. Coli (MPN/100mL)</label>
                                         <div class="col-sm-8">
-                                            <input id="ecoli" name="ecoli" type="text"  placeholder="E. Coli (MPN/100mL)" class="form-control">
+                                            <input id="ecoli" name="ecoli" type="text"  placeholder="E. Coli (MPN/100mL)" class="form-control" readonly>
                                             <!-- <div class="val1tip"></div> -->
                                         </div>
                                     </div>
@@ -294,7 +294,7 @@
                                     <div class="form-group">
                                         <label for="lowerconfidence" class="col-sm-4 control-label">Lower confidence limit MPN/100 mL</label>
                                         <div class="col-sm-8">
-                                            <input id="lowerconfidence" name="lowerconfidence" type="text"  placeholder="Lower confidence limit MPN/100 mL" class="form-control">
+                                            <input id="lowerconfidence" name="lowerconfidence" type="text"  placeholder="Lower confidence limit MPN/100 mL" class="form-control" readonly>
                                             <!-- <div class="val1tip"></div> -->
                                         </div>
                                     </div>
@@ -316,7 +316,7 @@
                                     <div class="form-group">
                                         <label for="coliforms" class="col-sm-4 control-label">Coliforms (MPN/100mL)</label>
                                         <div class="col-sm-8">
-                                            <input id="coliforms" name="coliforms" type="text"  placeholder="Coliforms (MPN/100mL)" class="form-control">
+                                            <input id="coliforms" name="coliforms" type="text"  placeholder="Coliforms (MPN/100mL)" class="form-control" readonly>
                                             <!-- <div class="val1tip"></div> -->
                                         </div>
                                     </div>
@@ -1155,32 +1155,137 @@
         });
 
 
+        // function datachart(valueLargeWells, valueSmallWells) {
+        //     $.ajax({
+        //         type: "GET",
+        //         url: `${BASE_URL}/Colilert_hemoflow/data_chart?valueLargeWells=`+valueLargeWells+"&valueSmallWells="+valueSmallWells,
+        //         dataType: "json",
+        //         success: function(data) {
+        //             console.log('data mpm '+ data);
+        //             if (data.length > 0) {
+        //                 if (data[0].mpn == '<Detection') {
+        //                     result = "<"+ Math.round(1 / dilution);
+        //                 }
+        //                 else if (data[0].mpn == '>Detection') {
+        //                     result = ">"+ Math.round(2082 / dilution);
+        //                 }
+        //                 else {
+        //                     result = Math.round(data[0].mpn / dilution);
+        //                 }
+        //             }
+        //             else {
+        //                 result = 'Invalid';     
+        //             }
+        //         }
+        //     });
+            
+        //     return result; 
+        // }
+
+        // Cache untuk menyimpan hasil calculation
+        let calculationCache = {};
+        let debounceTimer = null;
+        
         function datachart(valueLargeWells, valueSmallWells) {
+            // Create cache key
+            const cacheKey = `${valueLargeWells}_${valueSmallWells}`;
+            
+            // Check cache first
+            if (calculationCache[cacheKey]) {
+                console.log('Using cached result for:', cacheKey);
+                return calculationCache[cacheKey];
+            }
+            
+            let result = { mpn: 'Invalid', lower: 'Invalid' };
             $.ajax({
                 type: "GET",
                 url: `${BASE_URL}/Colilert_hemoflow/data_chart?valueLargeWells=`+valueLargeWells+"&valueSmallWells="+valueSmallWells,
                 dataType: "json",
+                async: false, // Make synchronous to get result immediately
                 success: function(data) {
-                    console.log('data mpm '+ data);
+                    console.log('data mpn: ', data);
                     if (data.length > 0) {
-                        if (data[0].mpn == '<Detection') {
-                            result = "<"+ Math.round(1 / dilution);
+                        if (data[0].MPN_mean == '0') {
+                            let calculatedMpn = parseFloat(data[0].MPN_mean) / parseFloat(dilution);
+                            let calculatedLower = parseFloat(data[0].MPN_95lo) / parseFloat(dilution);
+                            result.mpn = calculatedMpn.toFixed(1);
+                            result.lower = calculatedLower.toFixed(1);
                         }
-                        else if (data[0].mpn == '>Detection') {
-                            result = ">"+ Math.round(2082 / dilution);
+                        else if (data[0].MPN_mean == '9999') {
+                            result.mpn = ">"+ (2419 / dilution).toFixed(1);
+                            let calculatedLower = parseFloat(data[0].MPN_95lo) / parseFloat(dilution);
+                            result.lower = calculatedLower.toFixed(1);
                         }
                         else {
-                            result = Math.round(data[0].mpn / dilution);
+                            // Format to 1 decimal place for display
+                            let calculatedMpn = parseFloat(data[0].MPN_mean) / parseFloat(dilution);
+                            let calculatedLower = parseFloat(data[0].MPN_95lo) / parseFloat(dilution);
+                            result.mpn = calculatedMpn.toFixed(1);
+                            result.lower = calculatedLower.toFixed(1);
                         }
+                        
+                        // Store in cache
+                        calculationCache[cacheKey] = result;
                     }
                     else {
-                        result = 'Invalid';     
+                        result = { mpn: 'Invalid', lower: 'Invalid' };     
                     }
+                },
+                error: function() {
+                    result = { mpn: 'Invalid', lower: 'Invalid' };
                 }
             });
             
             return result; 
         }
+
+        // Debounced calculation function
+        function performCalculation(largeWellsId, smallWellsId, targetFields) {
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(function() {
+                let empnResult = datachart($(largeWellsId).val(), $(smallWellsId).val());
+                if (empnResult.mpn == 'Invalid'){
+                    targetFields.forEach(field => {
+                        $(field).css({'background-color' : '#FFE6E7'});
+                    });
+                    $(largeWellsId).val('');
+                    $(smallWellsId).val('');
+                }
+                else {
+                    targetFields.forEach(field => {
+                        $(field).css({'background-color' : '#EEEEEE'});
+                    });
+                    
+                    // Set values based on target fields
+                    if (targetFields.includes("#ecoli")) {
+                        $("#ecoli").val(empnResult.mpn);
+                        $("#lowerconfidence").val(empnResult.lower);
+                    }
+                    if (targetFields.includes("#coliforms")) {
+                        $("#coliforms").val(empnResult.mpn);
+                    }
+                }
+            }, 300); // 300ms delay
+        }
+
+        // E. coli calculation functions with debouncing
+        $('#ecoli_largewells').on('input change', function(event) {        
+            performCalculation('#ecoli_largewells', '#ecoli_smallwells', ['#ecoli', '#lowerconfidence']);
+        });
+
+        $('#ecoli_smallwells').on('input change', function(event) {        
+            performCalculation('#ecoli_largewells', '#ecoli_smallwells', ['#ecoli', '#lowerconfidence']);
+        });
+
+        // Total coliforms calculation functions with debouncing
+        $('#coliforms_largewells').on('input change', function(event) {        
+            performCalculation('#coliforms_largewells', '#coliforms_smallwells', ['#coliforms']);
+        });
+
+        $('#coliforms_smallwells').on('input change', function(event) {        
+            performCalculation('#coliforms_largewells', '#coliforms_smallwells', ['#coliforms']);
+        });
+
 
         // $('#ecoli_largewells').on('change keypress keyup keydown', function(event) {        
         //     let empn = datachart($('#ecoli_largewells').val(), $('#ecoli_smallwells').val());
@@ -1423,13 +1528,13 @@
             $('#colilert_hemoflow_barcodex').val(colilertBarcode);
             $('#colilert_hemoflow_barcodex').attr('readonly', true);
             $('#idx_colilert_hemoflow').val(idColilertHemoflow);
-            $('#ecoli_largewells').val('0');
-            $('#ecoli_smallwells').val('0');
-            $('#ecoli').val('');
+            $('#ecoli_largewells').val('');
+            $('#ecoli_smallwells').val('');
+            $('#ecoli').val('0');
             $('#lowerconfidence').val('0');
-            $('#coliforms_largewells').val('0');
-            $('#coliforms_smallwells').val('0');
-            $('#coliforms').val('');
+            $('#coliforms_largewells').val('');
+            $('#coliforms_smallwells').val('');
+            $('#coliforms').val('0');
             $('#remarks').val('');
             // Reset quality control checkbox for new record
             $('#quality_control_ciw').prop('checked', false);
